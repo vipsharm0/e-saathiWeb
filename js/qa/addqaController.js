8822ed7b194acdbf7c71f98ce711de8abb2a9b16
Vipin S <vsharma98@sapient.com>
2018-08-16 14:45:45 +0530

    catagory and error placeholder added

--- js/qa/addqaController.js
+++ js/qa/addqaController.js
@@ -1,15 +1,22 @@
 var app = angular.module('saathiApp')
 
-.controller('qsAddController', ['$scope',function($scope){ 
+.controller('qsAddController', ['$scope', 'saathiService',function($scope, saathiService){ 
     $scope.answerset = {answers:[]};
     $scope.answerset.answers = []
-   
+    $scope.answerset.answers.push('');
+  
+
     $scope.addAnswer = function () {
         $scope.answerset.answers.push('');
     };
 
     $scope.removeAnswer = function (z) {
         //var lastItem = $scope.choiceSet.choices.length - 1;
-        $scope.answerset.answers.splice(z,1);
-    };
+        if($scope.answerset.answers.length > 1){
+            $scope.answerset.answers.splice(z,1);
+        }
+        
+    }
+    
+    
 }])
